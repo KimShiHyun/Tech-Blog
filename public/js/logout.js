98bd79558bf85b113942a944f3e01c454504f51f
Kevin Kim <kevin.shihyun@gmail.com>
2021-10-11 19:28:40 -0600

    public js files

---
+++ public/js/logout.js
@@ -0,0 +1,15 @@
+async function logout() {
+  const response = await fetch("/api/users/logout", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    alert("You have been logged off.");
+    document.location.replace("/");
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector("#logout").addEventListener("click", logout);
